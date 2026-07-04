b276ba3bc1f4509d99ee1dd69061947e91952f8b
yuz14 <yuz145@icloud.com>
2026-07-04 20:09:20 +0900

    a

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ const CONFIG = {
   // wrangler deploy で発行されたWorkersのURLに置き換える
   // 例: https://oyuzen.your-subdomain.workers.dev/api/notes
   indexUrl: "https://pub-e5e3bb4f567c42cabe57034c606e7b3b.r2.dev",
-  entryBaseUrl: "https://oyuzen.<subdomain>.workers.dev/api/notes/",
+  entryBaseUrl: "https://notes-api.yuz145.workers.dev/api/notes/",
 };
